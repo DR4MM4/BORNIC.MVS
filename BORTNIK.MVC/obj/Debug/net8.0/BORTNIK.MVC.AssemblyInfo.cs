f6cd74305fdd246c7bfbd5d40cb8838726896a3a
szqwyyu <sn4ik231@gmail.com>
2025-11-04 16:14:34 +0200

    123

--- BORTNIK.MVC/obj/Debug/net8.0/BORTNIK.MVC.AssemblyInfo.cs
+++ BORTNIK.MVC/obj/Debug/net8.0/BORTNIK.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BORTNIK.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b916c2ce4252264cee8d332011bc0a7600868bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BORTNIK.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BORTNIK.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
